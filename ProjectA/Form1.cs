dda381ec6de588d668db388f0b87c4ad73e7ce68
KubaaKs <jakubksiazka@wp.pl>
2020-05-20 17:45:33 +0200

    Dodanie funkcji Wygrana i Przegrana informująca gracza o wyniku rozgrywki

--- ProjectA/Form1.cs
+++ ProjectA/Form1.cs
@@ -15,7 +15,7 @@ namespace ProjectA
         public Form1()
         {
             InitializeComponent();
-            
+
 
 
         }
@@ -29,7 +29,7 @@ namespace ProjectA
         int lewo = -1;
         int gora = 0;
         int cts = 0;
-        int pts = 0;
+        int pts = 0; /// punkty
 
         bool gra = true;
         bool ruszLewo;
@@ -38,15 +38,15 @@ namespace ProjectA
 
         private void Pressed(object sender, KeyEventArgs e)
         {
-            if(e.KeyCode == Keys.A || e.KeyCode == Keys.Left)
+            if (e.KeyCode == Keys.A || e.KeyCode == Keys.Left)
             {
                 ruszLewo = true;
             }
-            else if(e.KeyCode == Keys.D || e.KeyCode == Keys.Right)
+            else if (e.KeyCode == Keys.D || e.KeyCode == Keys.Right)
             {
-                ruszPrawo = true; 
+                ruszPrawo = true;
             }
-            else if(e.KeyCode == Keys.Space && gra && !Strzal)
+            else if (e.KeyCode == Keys.Space && gra && !Strzal)
             {
                 Missle();
                 Strzal = true;
@@ -55,15 +55,15 @@ namespace ProjectA
 
         private void NoPressed(object sender, KeyEventArgs e)
         {
-            if(e.KeyCode == Keys.A || e.KeyCode == Keys.Left)
+            if (e.KeyCode == Keys.A || e.KeyCode == Keys.Left)
             {
                 ruszLewo = false;
             }
-            else if(e.KeyCode == Keys.D || e.KeyCode == Keys.Right)
+            else if (e.KeyCode == Keys.D || e.KeyCode == Keys.Right)
             {
                 ruszPrawo = false;
             }
-            else if(e.KeyCode = Keys.Space)
+            else if (e.KeyCode = Keys.Space)
             {
                 Strzal = false;
             }
@@ -74,7 +74,7 @@ namespace ProjectA
         private void Missle()
         {
             PictureBox bullet = new PictureBox();
-            bullet.Location = new Point(Gracz.Location.X + Gracz.Width / 2, Gracz.Location.Y - 20)
+            bullet.Location = new Point(Gracz.Location.X + Gracz.Width / 2, Gracz.Location.Y - 20);
             bullet.Size = new Size(5, 20);
             bullet.BackgroundImage = Properties.Resources.bullet;
             bullet.BackgroundImageLayout = ImageLayout.Stretch;
@@ -82,5 +82,72 @@ namespace ProjectA
             this.Controls.Add(bullet);
 
         }
+
+        private void GraczRuch(object sender, EventArgs e)
+        {
+            if (ruszLewo && Gracz.Location.X >= 0)
+            {
+                Gracz.Left--;
+            }
+
+
+            else if (ruszPrawo && Gracz.Location.X <= limit)
+            {
+                Gracz.Left++;
+            }
+        }
+
+        private void SprawdzWygrana()
+        {
+            int licznik = 0;
+            foreach(Control c in this.Controls)
+            {
+                if (c is PictureBox && c.Name = "Wrog") licznik++;
+            }
+
+            if (licznik == 0) Wygrales();
+
+        }
+
+        private void Wygrales()
+        {
+            gra = false;
+
+            foreach(Control c in this.Controls)
+            {
+                if(c is Label && c.Name ="Koniec")
+                {
+                    Label lbl = (Label)c;
+                    lbl.Text = "Wygrales !" + "\n"
+                        + "Wynik: " + pts.ToString();
+                }
+                else
+                {
+                    c.Visible = false;
+                }
+            }
+
+        }
+
+        private void Przegrales()
+        {
+            timer1.Stop(); timer2.Stop(); timer3.Stop(); timer4.Stop(); timer5.Stop(); Observer.Stop();
+
+            foreach(Control c in this.Controls)
+            {
+                if(c is Label && c.Name == "Finish")
+                {
+                    Label lbl = (Label)c;
+                    lbl.Text = "Przegrales! ";
+                    gra = false;
+                }
+                else
+                {
+                    c.Visible = false;
+                }
+            }
+        }
+
+           
     }
 }
